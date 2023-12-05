82374b2ce35a5aa658e16de55c66b478fe6d9807
Minh Kiet <kietminh070502@gmail.com>
2023-12-05 20:32:04 +0700

    Admin page - Pages: Delete Page

---
+++ admin/modules/pages/delete.php
@@ -0,0 +1,28 @@
+<?php
+if (!defined('_INCODE')) die('Access Deined...');
+$body = getBody();
+if (!empty($body['id'])) {
+  $pageId = $body['id'];
+  $pageDetailRows = getRows("SELECT id FROM pages WHERE id=$pageId");
+  if ($pageDetailRows > 0) {
+
+    //Thực hiện xoá
+    $condition = "id=$pageId";
+    $deleteStatus = delete('pages', $condition);
+    if (!empty($deleteStatus)) {
+      setFlashData('msg', 'Xoá trang thành công');
+      setFlashData('msg_type', 'success');
+    } else {
+      setFlashData('msg', 'Xoá trang không thành công. Vui lòng thử lại sau');
+      setFlashData('msg_type', 'danger');
+    }
+  } else {
+    setFlashData('msg', 'Trang không tồn tại trên hệ thống');
+    setFlashData('msg_type', 'danger');
+  }
+} else {
+  setFlashData('msg', 'Liên kết không tồn tại');
+  setFlashData('msg_type', 'danger');
+}
+
+redirect('admin?module=pages');
