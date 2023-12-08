63ca6f32c9c98993616297fdcf6ea53dc8b6fd48
Minh Kiet <kietminh070502@gmail.com>
2023-12-08 22:50:23 +0700

    Admin page - Portfolio Categories: Duplicate Category

---
+++ admin/modules/portfolio_categories/duplicate.php
@@ -0,0 +1,45 @@
+<?php
+if (!defined('_INCODE')) die('Access Deined...');
+$body = getBody();
+if (!empty($body['id'])) {
+  $pageId = $body['id'];
+  $cateDetail = firstRaw("SELECT * FROM portfolio_categories WHERE id=$pageId");
+  if (!empty($cateDetail)) {
+
+    //Loại bỏ thời gian tạo (create_at), thời gian cập nhật (update_at), id
+    $cateDetail['create_at'] = date('Y-m-d H:i:s');
+
+    unset($cateDetail['update_at']);
+
+    unset($cateDetail['id']);
+
+    $duplicate = $cateDetail['duplicate'];
+    $duplicate++;
+
+    $name = $cateDetail['name'] . ' (' . $duplicate . ')';
+
+    $cateDetail['name'] = $name;
+
+    $insertStatus = insert('portfolio_categories', $cateDetail);
+    if ($insertStatus) {
+      setFlashData('msg', 'Nhân bản danh mục thành công');
+      setFlashData('msg_type', 'success');
+
+      update(
+        'portfolio_categories',
+        [
+          'duplicate' => $duplicate
+        ],
+        "id=$pageId"
+      );
+    }
+  } else {
+    setFlashData('msg', 'Danh mục không tồn tại trên hệ thống');
+    setFlashData('msg_type', 'danger');
+  }
+} else {
+  setFlashData('msg', 'Liên kết không tồn tại');
+  setFlashData('msg_type', 'danger');
+}
+
+redirect('admin?module=portfolio_categories');
