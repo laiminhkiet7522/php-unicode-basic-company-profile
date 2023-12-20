58b92303bfd3287affb5d484ba8b5980f2531ea0
Minh Kiet <kietminh070502@gmail.com>
2023-12-20 08:51:23 +0700

    Admin page - Blog: Duplicate Blog

---
+++ admin/modules/blog/duplicate.php
@@ -0,0 +1,45 @@
+<?php
+if (!defined('_INCODE')) die('Access Deined...');
+$body = getBody();
+if (!empty($body['id'])) {
+  $blogId = $body['id'];
+  $blogDetail = firstRaw("SELECT * FROM blog WHERE id=$blogId");
+  if (!empty($blogDetail)) {
+
+    //Loại bỏ thời gian tạo (create_at), thời gian cập nhật (update_at), id
+    $blogDetail['create_at'] = date('Y-m-d H:i:s');
+
+    unset($blogDetail['update_at']);
+
+    unset($blogDetail['id']);
+
+    $duplicate = $blogDetail['duplicate'];
+    $duplicate++;
+
+    $name = $blogDetail['title'] . ' (' . $duplicate . ')';
+
+    $blogDetail['title'] = $name;
+
+    $insertStatus = insert('blog', $blogDetail);
+    if ($insertStatus) {
+      setFlashData('msg', 'Nhân bản blog thành công');
+      setFlashData('msg_type', 'success');
+
+      update(
+        'blog',
+        [
+          'duplicate' => $duplicate
+        ],
+        "id=$blogId"
+      );
+    }
+  } else {
+    setFlashData('msg', 'Blog không tồn tại trên hệ thống');
+    setFlashData('msg_type', 'danger');
+  }
+} else {
+  setFlashData('msg', 'Liên kết không tồn tại');
+  setFlashData('msg_type', 'danger');
+}
+
+redirect('admin?module=blog');
