9065907cdd5a8e4729948cf51e8025d0838cab6a
Minh Kiet <kietminh070502@gmail.com>
2023-12-27 08:15:02 +0700

    Admin page - Portfolios Image: Delete Photo Library

--- admin/modules/portfolios/delete.php
+++ admin/modules/portfolios/delete.php
@@ -1,28 +1,30 @@
 <?php
 if (!defined('_INCODE')) die('Access Deined...');
 $body = getBody();
-if (!empty($body['id'])){
+if (!empty($body['id'])) {
     $portfolioId = $body['id'];
     $portfolioDetailRows = getRows("SELECT id FROM portfolios WHERE id=$portfolioId");
-    if ($portfolioDetailRows>0){
+    if ($portfolioDetailRows > 0) {
+
+        //Xử lý xóa thư viện ảnh
+        delete('portfolio_images', 'portfolio_id=' . $body['id']);
 
         //Thực hiện xoá
         $condition = "id=$portfolioId";
-
         $deleteStatus = delete('portfolios', $condition);
-        if (!empty($deleteStatus)){
+
+        if (!empty($deleteStatus)) {
             setFlashData('msg', 'Xoá dự án thành công');
             setFlashData('msg_type', 'success');
-        }else{
+        } else {
             setFlashData('msg', 'Xoá dự án không thành công. Vui lòng thử lại sau');
             setFlashData('msg_type', 'danger');
         }
-
-    }else{
+    } else {
         setFlashData('msg', 'Dự án không tồn tại trên hệ thống');
         setFlashData('msg_type', 'danger');
     }
-}else{
+} else {
     setFlashData('msg', 'Liên kết không tồn tại');
     setFlashData('msg_type', 'danger');
 }
