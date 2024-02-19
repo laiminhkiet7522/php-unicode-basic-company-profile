969721523d34e06ef45ffcb05ff2e2df7a419739
Minh Kiet <laiminhkiet07052002@gmail.com>
2024-02-19 20:34:30 +0700

    Front Page: Blog - Blog details - Part 2

--- modules/blog/detail.php
+++ modules/blog/detail.php
@@ -8,7 +8,7 @@ if (!empty(getBody()['id'])) {
   loadError('404');
 }
 
-$parentBreadcrumb = '<li><a href="' . _WEB_HOST_ROOT . '?module=blog' . '">'.getOption('blog_title').'</a></li>';
+$parentBreadcrumb = '<li><a href="' . _WEB_HOST_ROOT . '?module=blog' . '">' . getOption('blog_title') . '</a></li>';
 $parentBreadcrumb .= '<li><a href="' . _WEB_HOST_ROOT . '?module=blog&action=category&id=' . $blogDetail['cate_id'] . '">' . $blogDetail['cate_name'] . '</a></li>';
 
 $data = [
@@ -29,29 +29,33 @@ layout('breadcrumb', 'client', $data);
           <div class="col-12">
             <!-- Single Blog -->
             <div class="single-blog">
-              <div class="blog-head">
-                <img src="images/blogs/blog1.jpg" alt="#">
-              </div>
+              <?php
+              if (!empty($blogDetail['thumbnail'])) :
+              ?>
+                <div class="blog-head">
+                  <img src="<?php echo $blogDetail['thumbnail']; ?>" alt="#">
+                </div>
+              <?php endif; ?>
               <div class="blog-inner">
                 <div class="blog-top">
                   <div class="meta">
-                    <span><i class="fa fa-bolt"></i><a href="#">Marketing</a></span>
-                    <span><i class="fa fa-calendar"></i>03 May, 2018</span>
-                    <span><i class="fa fa-eye"></i><a href="#">333k</a></span>
+                    <span><i class="fa fa-bolt"></i><a href="<?php echo _WEB_HOST_ROOT . '?module=blog&action=category&id=' . $blogDetail['cate_id']; ?>"><?php echo $blogDetail['cate_name']; ?></a></span>
+                    <span><i class="fa fa-calendar"></i><?php echo getDateFormat($blogDetail['create_at'], 'd M, Y') ?></span>
+                    <span><i class="fa fa-eye"></i><?php echo $blogDetail['view_count']; ?></span>
                   </div>
                   <ul class="social-share">
-                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                    <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo _WEB_HOST_ROOT . '?module=blog&action=detail&id=' . $id; ?>" target="_blank"><i class="fa fa-facebook"></i></a></li>
+
+                    <li><a href="http://twitter.com/share?text=<?php echo $blogDetail['title'] . '&url=' . _WEB_HOST_ROOT . '?module=blog&action=detail&id=' . $id; ?>" target="_blank"><i class="fa fa-twitter"></i></a></li>
+
+                    <li><a href="https://www.linkedin.com/sharing/share-offsite/?url=<?php echo _WEB_HOST_ROOT . '?module=blog&action=detail&id=' . $id; ?>" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+
+                    <li><a href="http://pinterest.com/pin/create/button/?url=<?php echo _WEB_HOST_ROOT . '?module=blog&action=detail&id=' . $id; ?>" target="_blank"><i class="fa fa-pinterest"></i></a></li>
+
                   </ul>
                 </div>
-                <h2><a href="blog-single.html">Recognizing the need is the primary</a></h2>
-                <p>dolor sit amet, consectetur adipiscing elit. Fusce porttitor tristique mi, sed rhoncus sapien mollis vitae. Pellentesque at mauris neque. Vestibulum pulvinar ac sagittis ex consectetur sed. Ut viverra elementum libero, nec tincidunt orci vehicula quis. Vivamus vehicula quis <b>Lorem ipsum</b> nunc quis rutrum. Aliquam consectetur dapibus tortor, blandit lobortis erat dictum sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed vitae quam dolor.</p>
-                <p>sed eleifend lectus purus id sem. Morbi eget interdum ligula. Cras tincidunt tincidunt odio et accumsan. Aliquam erat volutpat. In iaculis tortor ac congue cursus. In hac habitasse platea dictumst. Maecenas eu dignissim nisi. Donec feugiat, massa vel egestas dapibus, libero purus lacinia eros, <u>magna enim</u> eu pellentesque lorem purus id orci. Cras tempor, mauris vitae congue sollicitudin, ex justo viverra ipsum, sit amet viverra justo odio ac metus. Aenean tristique odio id lectus accumsan convallis. Praesent tempor elit pulvinar elit ultricies, sed gravida nulla cursus. In condimentum mi ex, vel dapibus arcu accumsan ut. </p>
-                <blockquote>Trending Title in ullamcorper sollicitudin, ligula nisi hendrerit magna, eget rhoncus purus urna at risus. Nullam volutpat augue at orci malesuada sollicitudin ut id risus. Ut tincidunt, erat eget feugiat eleifend, eros magna dapibus diam, eu aliquam dolor ipsum fringilla nulla</blockquote>
-                <p>dolor sit amet, consectetur adipiscing elit. Fusce porttitor tristique mi, sed rhoncus sapien mollis vitae. Pellentesque at mauris neque. Vestibulum pulvinar ac sagittis ex consectetur sed. Ut viverra elementum libero, nec tincidunt orci vehicula quis. Vivamus vehicula quis nunc quis rutrum. Aliquam consectetur dapibus tortor, blandit lobortis erat dictum sed. Interdum et malesuada fames ac ante ipsum primis in .</p>
+                <h2><?php echo $blogDetail['title']; ?></h2>
+                <?php echo html_entity_decode($blogDetail['content']); ?>
                 <div class="bottom-area">
                   <!-- Next Prev -->
                   <ul class="arrow">
