002a482f907fe4b7101e08168e52d5ef8ac50c11
Minh Kiet <laiminhkiet07052002@gmail.com>
2024-02-22 09:49:03 +0700

    Front Page: Contacts - Submit contact form

--- modules/page-template/contact.php
+++ modules/page-template/contact.php
@@ -15,6 +15,73 @@ $twitter = getOption('general_twitter');
 $linkedin = getOption('general_linkedin');
 $behance = getOption('general_behance');
 $youtube = getOption('general_youtube');
+
+//Truy vấn lấy phòng ban
+$contactTypeLists = getRaw("SELECT * FROM contact_type ORDER BY name ASC");
+
+//Xử lý gửi liên hệ
+if (isPost()) {
+  $body = getBody();
+  $errors = [];
+
+  //Validate full name
+  if (empty(trim($body['fullname']))) {
+    $errors['fullname']['required'] = 'Full name is required';
+  } elseif (strlen(trim($body['fullname'])) < 5) {
+    $errors['name']['min'] = 'Full name must be at least 5 characters';
+  }
+
+  //Validate email
+  if (empty(trim($body['email']))) {
+    $errors['email']['required'] = 'Email is required';
+  } elseif (!isEmail(trim($body['email']))) {
+    $errors['email']['invalid'] = 'Email is invalid';
+  }
+
+  //Validate message
+  if (empty($body['message'])) {
+    $errors['message']['required'] = 'Please enter your message';
+  } else {
+    if (strlen(trim($body['message'])) < 10) {
+      $errors['message']['min'] = 'Message must be at least 10 characters';
+    }
+  }
+
+  if (empty($errors)) {
+    //Xử lý thêm liên hệ vào cơ sở dữ liệu
+    $dataInsert = [
+      'fullname' => trim(strip_tags($body['fullname'])),
+      'email' => trim(strip_tags($body['email'])),
+      'type_id' => $body['contact_type'],
+      'message' => trim(strip_tags($body['message'])),
+      'status' => 0,
+      'create_at' => date('Y-m-d H:i:s')
+    ];
+
+    $insertStatus = insert('contacts', $dataInsert);
+    if ($insertStatus) {
+      setFlashData('msg', 'Contact sent successfully');
+      setFlashData('msg_type', 'success');
+      redirect('?module=page-template&action=contact');
+    } else {
+      setFlashData('msg', 'Sending contact failed');
+      setFlashData('msg_type', 'danger');
+      setFlashData('errors', $errors);
+      setFlashData('old', $body);
+      redirect('?module=page-template&action=contact');
+    }
+  } else {
+    //Có lỗi xảy ra
+    setFlashData('msg', 'Please check the entered data');
+    setFlashData('msg_type', 'danger');
+    setFlashData('errors', $errors);
+    redirect('?module=page-template&action=contact');
+  }
+}
+$msg = getFlashData('msg');
+$msgType = getFlashData('msg_type');
+$errors = getFlashData('errors');
+
 ?>
 <!-- Start Contact -->
 <section id="contact-us" class="contact-us section">
@@ -38,31 +105,41 @@ $youtube = getOption('general_youtube');
                 <div class="text-content">
                   <h2>Send Message Us</h2>
                 </div>
-                <form class="form" method="post" action="mail/mail.php">
+                <?php
+                getMsg($msg, $msgType);
+                ?>
+                <form class="form" method="post" action="">
                   <div class="row">
                     <div class="col-lg-6 col-12">
                       <div class="form-group">
-                        <input type="text" name="name" placeholder="Full Name" required="required">
+                        <input type="text" name="fullname" placeholder="Full Name">
+                        <?php echo form_error('fullname', $errors, '<span class="error">', '</span>'); ?>
                       </div>
                     </div>
                     <div class="col-lg-6 col-12">
                       <div class="form-group">
-                        <input type="email" name="email" placeholder="Your Email" required="required">
+                        <input type="email" name="email" placeholder="Your Email">
+                        <?php echo form_error('email', $errors, '<span class="error">', '</span>'); ?>
                       </div>
                     </div>
                     <div class="col-12">
                       <div class="form-group">
-                        <select name="subject">
-                          <option class="option" value="1">Starting a new business</option>
-                          <option class="option" value="2">Startup Consultation</option>
-                          <option class="option" value="3">Financial Consultation</option>
-                          <option class="option" value="4">Business Consultation</option>
+                        <select name="contact_type">
+                          <?php
+                          if (!empty($contactTypeLists)) :
+                            foreach ($contactTypeLists as $item) : ?>
+                              <option class="option" value="<?php echo $item['id']; ?>"><?php echo $item['name']; ?></option>
+                          <?php
+                            endforeach;
+                          endif;
+                          ?>
                         </select>
                       </div>
                     </div>
                     <div class="col-lg-12 col-12">
                       <div class="form-group">
                         <textarea name="message" rows="6" placeholder="Type Your Message"></textarea>
+                        <?php echo form_error('message', $errors, '<span class="error">', '</span>'); ?>
                       </div>
                     </div>
                     <div class="col-lg-12 col-12">
